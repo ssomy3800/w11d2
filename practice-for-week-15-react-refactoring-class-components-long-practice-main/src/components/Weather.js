677f457aa69da41b160e96bb68b41e75c093dc54
ssomy3800 <ssomy3800@gmail.com>
2023-05-16 16:11:43 -0700

    finished 3rd part

--- practice-for-week-15-react-refactoring-class-components-long-practice-main/src/components/Weather.js
+++ practice-for-week-15-react-refactoring-class-components-long-practice-main/src/components/Weather.js
@@ -1,75 +1,151 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { toQueryString } from "../utils";
 
-// componentDidMount() {
-//       navigator.geolocation?.getCurrentPosition(
-//         this.pollWeather,
-  //       (err) => console.log(err),
-  //       { timeout: 10000 }
-  //     );
-  //   }
 function Weather() {
-  const [weather, setWeather] = useState({});
+  const [weather, setWeather] = useState(null);
+
   useEffect(() => {
-    console.log("component has mounted");
-    let url = "http://api.openweathermap.org/data/2.5/weather?";
     const pollWeather = async (location) => {
-      console.log("firing off async request");
+      let url = "http://api.openweathermap.org/data/2.5/weather?";
+
+      /* Remember that it's unsafe to expose your API key. (Note that pushing
+      files that include your key to Github will expose your key!) In
+      production, you would definitely save your key in an environment variable,
+      so do that here. Since this project runs in your local environment
+      (localhost), save your key as an environment variable in a .env file in
+      the root directory of your app. You can then access the key here as
+      "process.env.<variable_name>". Make sure to .gitignore your .env file!
+      Also remember to restart your server (i.e., re-run "npm start") whenever
+      you change your .env file. */
       const apiKey = process.env.REACT_APP_WEATHER_API;
-      debugger;
+
       const params = {
         lat: location.coords.latitude,
         lon: location.coords.longitude,
-        // lat: 40.730610, 
-        // lon: -73.935242,
         appid: apiKey,
       };
-      console.log(params);
+
       url += toQueryString(params);
-      try {
-        let res = await fetch(url);
-        if (res.ok) {
-          debugger;
-          let data = await res.json();
-          debugger;
-          setWeather(data);
-          debugger;
-        } else {
-          throw res;
-        }
-      } catch {
-        console.error("nice try");
+
+      const res = await fetch(url);
+      if (res.ok) {
+        const weather = await res.json();
+        setWeather(weather);
+      } else {
+        alert("Check Weather API key!");
       }
     };
 
-    const timeRequest = setInterval(pollWeather, 1000); // returns interval object
-
-    return () => {
-      clearInterval(timeRequest);
-      console.log("unmounting the component!");
-    };
+    navigator.geolocation?.getCurrentPosition(
+      pollWeather,
+      (err) => console.log(err),
+      { timeout: 10000 }
+    );
   }, []);
 
-  return () => {
-    if (weather) {
-      const temp = (weather.main.temp - 273.15) * 1.8 + 32;
-      content = (
-        <div>
-          <p>{weather.name}</p>
-          <p>{temp.toFixed(1)} degrees</p>
-        </div>
-      );
-    } else {
-      content = (
-        <div>
-          Weather is currently unavailable. (Are Location Services enabled?)
-        </div>
-      );
-    }
-  };
+  let content = <div className="loading">loading weather...</div>;
+
+  if (weather) {
+    const temp = (weather.main.temp - 273.15) * 1.8 + 32;
+    content = (
+      <div>
+        <p>{weather.name}</p>
+        <p>{temp.toFixed(1)} degrees</p>
+      </div>
+    );
+  } else {
+    content = (
+      <div>
+        Weather is currently unavailable. (Are Location Services enabled?)
+      </div>
+    );
+  }
+
+  return (
+    <section className="weather-section">
+      <h1>Weather</h1>
+      <div className="weather">{content}</div>
+    </section>
+  );
 }
 
 export default Weather;
+
+// import React, { useState, useEffect } from "react";
+// import { toQueryString } from "../utils";
+
+// // componentDidMount() {
+// //       navigator.geolocation?.getCurrentPosition(
+// //         this.pollWeather,
+// //       (err) => console.log(err),
+// //       { timeout: 10000 }
+// //     );
+// //   }
+// function Weather() {
+//   const [weather, setWeather] = useState({});
+//   useEffect(() => {
+//     navigator.geolocation?.getCurrentPosition(
+//       pollWeather,
+//       (err) => console.log(err),
+//       { timeout: 10000 }
+//     );
+//     let url = "http://api.openweathermap.org/data/2.5/weather?";
+//     const pollWeather = async () => {
+//       console.log("firing off async request");
+//       const apiKey = process.env.REACT_APP_WEATHER_API;
+//       debugger;
+//       const params = {
+//         lat: location.coords.latitude,
+//         lon: location.coords.longitude,
+
+//         appid: apiKey,
+//       };
+//       console.log(params);
+//       url += toQueryString(params);
+//       try {
+//         let res = await fetch(url);
+//         if (res.ok) {
+//           debugger;
+//           let data = await res.json();
+//           debugger;
+//           setWeather(data);
+//           debugger;
+//         } else {
+//           throw res;
+//         }
+//       } catch {
+//         console.error("nice try");
+//       }
+//     };
+
+//     const timeRequest = setInterval(pollWeather, 1000); // returns interval object
+
+//     return () => {
+//       clearInterval(timeRequest);
+//       console.log("unmounting the component!");
+//     };
+//   }, []);
+
+//   return () => {
+//     if (weather) {
+//       const temp = (weather.main.temp - 273.15) * 1.8 + 32;
+//       content = (
+//         <div>
+//           <p>{weather.name}</p>
+//           <p>{temp.toFixed(1)} degrees</p>
+//         </div>
+//       );
+//     } else {
+//       content = (
+//         <div>
+//           Weather is currently unavailable. (Are Location Services enabled?)
+//         </div>
+//       );
+//     }
+//   };
+// }
+
+// export default Weather;
 
 // class Weather extends React.Component {
 //   constructor(props) {
