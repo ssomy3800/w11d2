91f111e9e6a63a10b7ed2486580f45d6b70477f1
ssomy3800 <ssomy3800@gmail.com>
2023-05-16 21:08:44 -0700

    on part 4

--- practice-for-week-15-react-refactoring-class-components-long-practice-main/src/components/Clock.js
+++ practice-for-week-15-react-refactoring-class-components-long-practice-main/src/components/Clock.js
@@ -1,77 +1,133 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-export class ClockToggle extends React.Component {
-  render () {
-    return (
-      <button 
-        type="button"
-        className="clock-toggle" 
-        onClick={this.props.toggleClock}
-      >
-        Toggle Clock
-      </button>
-    )
-  }
-} 
+export function ClockToggle({ toggleClock }) {
+  return (
+    <button type="button" className="clock-toggle" onClick={toggleClock}>
+      Toggle Clock
+    </button>
+  );
+}
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      time: new Date(),
+function Clock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(tick, 1000);
+    return () => {
+      console.log("Clearing Clock interval!");
+      clearInterval(interval);
     };
-  }
-  
-  componentDidMount() {
-    this.interval = setInterval(this.tick, 1000);
-  }
-  
-  componentWillUnmount() {
-    console.log("Clearing Clock interval!")
-    clearInterval(this.interval);
-  }
-  
-  tick = () => {
-    this.setState({ time: new Date() });
-  }
+  }, []);
 
-  render() {
-    let hours = this.state.time.getHours();
-    let minutes = this.state.time.getMinutes();
-    let seconds = this.state.time.getSeconds();
-    hours = (hours < 10) ? `0${hours}` : hours;
-    minutes = (minutes < 10) ? `0${minutes}` : minutes;
-    seconds = (seconds < 10) ? `0${seconds}` : seconds;
+  const tick = () => {
+    setTime(new Date());
+  };
 
-    const timezone = this.state.time
-      .toTimeString() // Form: "14:39:07 GMT-0600 (PDT)"
-      .replace(/[^A-Z]/g, "") // Strip out all but capitals
-      .slice(3); // Eliminate initial GMT
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+  let seconds = time.getSeconds();
+  hours = hours < 10 ? `0${hours}` : hours;
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
 
-    return (
-      <section className="clock-section">
-        <h1>Clock</h1>
-        <div className='clock'>
-          <p className="time">
-            <span>
-              Time:
-            </span>
-            <span>
-              {`${hours}:${minutes}:${seconds} ${timezone}`}
-            </span>
-          </p>
-          <p className="date">
-            <span>
-              Date: 
-            </span>
-            <span>
-              {this.state.time.toDateString()}
-            </span>
-          </p>
-        </div>
-      </section>
-    );
-  }
+  const timezone = time
+    .toTimeString()
+    .replace(/[^A-Z]/g, "")
+    .slice(3);
+
+  return (
+    <section className="clock-section">
+      <h1>Clock</h1>
+      <div className="clock">
+        <p className="time">
+          <span>Time:</span>
+          <span>{`${hours}:${minutes}:${seconds} ${timezone}`}</span>
+        </p>
+        <p className="date">
+          <span>Date:</span>
+          <span>{time.toDateString()}</span>
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default Clock;
+
+// import React from 'react';
+
+// export class ClockToggle extends React.Component {
+//   render () {
+//     return (
+//       <button
+//         type="button"
+//         className="clock-toggle"
+//         onClick={this.props.toggleClock}
+//       >
+//         Toggle Clock
+//       </button>
+//     )
+//   }
+// }
+
+// class Clock extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       time: new Date(),
+//     };
+//   }
+
+//   componentDidMount() {
+//     this.interval = setInterval(this.tick, 1000);
+//   }
+
+//   componentWillUnmount() {
+//     console.log("Clearing Clock interval!")
+//     clearInterval(this.interval);
+//   }
+
+//   tick = () => {
+//     this.setState({ time: new Date() });
+//   }
+
+//   render() {
+//     let hours = this.state.time.getHours();
+//     let minutes = this.state.time.getMinutes();
+//     let seconds = this.state.time.getSeconds();
+//     hours = (hours < 10) ? `0${hours}` : hours;
+//     minutes = (minutes < 10) ? `0${minutes}` : minutes;
+//     seconds = (seconds < 10) ? `0${seconds}` : seconds;
+
+//     const timezone = this.state.time
+//       .toTimeString() // Form: "14:39:07 GMT-0600 (PDT)"
+//       .replace(/[^A-Z]/g, "") // Strip out all but capitals
+//       .slice(3); // Eliminate initial GMT
+
+//     return (
+//       <section className="clock-section">
+//         <h1>Clock</h1>
+//         <div className='clock'>
+//           <p className="time">
+//             <span>
+//               Time:
+//             </span>
+//             <span>
+//               {`${hours}:${minutes}:${seconds} ${timezone}`}
+//             </span>
+//           </p>
+//           <p className="date">
+//             <span>
+//               Date:
+//             </span>
+//             <span>
+//               {this.state.time.toDateString()}
+//             </span>
+//           </p>
+//         </div>
+//       </section>
+//     );
+//   }
+// }
+
+// export default Clock;
